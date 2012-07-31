bc12014c516a3791c5f362ea0e774f7dafebc2bd
Rashad Abdul-Salaam <rashad@getskinclick.com>
2012-07-30 21:25:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '8ceecfed8a43f3ce345879d76c8676d7e626265d0c1898faa90af0171c882e66b8140b3f585b2841a29f16834432e9c2729ba62f381c4d1d4500e63c2e3056e1'
