ada9da9454d051ffba116ceb3aff1964925a302c
Rashad Abdul-Salaam <rashad@getskinclick.com>
2012-08-04 00:21:08 -0500

    Add Contact Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,4 +43,17 @@ describe "Static pages" do
                     :text => "HappyHealth - Concierge Patient Check-in | About Us")
     end
   end
+    describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                    :text => "HappyHealth - Concierge Patient Check-in | Contact")
+    end
+  end
 end
